9670793f679506f7defe803b684b149e2c3c3acb
soongjamm <kimstz0303@gmail.com>
2022-11-01 23:50:29 +0900

    init

---
+++ src/test/java/com/soongjamm/eventsourcing/EventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongjamm.eventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
